4412e1d0d71ea010869acdba55b6925fde4656a1
hal255 <hluu114@gmail.com>
2016-09-07 11:19:32 -0700

    Added shape detection example

---
+++ RoboSUB_ImageProcess/src/examples/shape_detection_example.py
@@ -0,0 +1,70 @@
+'''
+Created on Sep 2, 2016
+
+This example program is used to detect shapes from an image
+
+@author: Hong Luu
+'''
+
+# import numpy as np
+# import cv2
+# im = cv2.imread("../resources/shapes.jpg")
+# imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# ret,thresh = cv2.threshold(imgray,127,255,0)
+# im2, contours, hierarchy = cv2.findContours(
+#     thresh.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# 
+# cv2.imshow("shapes", im)
+# cv2.imshow("gray", imgray)
+# cv2.imshow("im2", im2)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+import numpy as np
+import cv2
+
+img = cv2.imread('../resources/black-and-white-shapes.png')
+gray = cv2.imread('../resources/black-and-white-shapes.png',0)
+
+# img = cv2.imread('../resources/shapes.jpg')
+# gray = cv2.imread('../resources/shapes.jpg',0)
+
+ret,thresh = cv2.threshold(gray,127,255,0)
+
+contours,h = cv2.findContours(thresh,1,2)
+
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+    print len(approx)
+    if len(approx)==3:
+        print "triangle"
+        cv2.drawContours(img,[cnt],0,(0,0,255),-1)      # red for triangle
+    elif len(approx)==4:
+        print "square"
+        cv2.drawContours(img,[cnt],0,(0,165,255),-1)    # orange for square
+    elif len(approx)==5:
+        print "pentagon"
+        cv2.drawContours(img,[cnt],0,(0,255,255),-1)    # yellow for pentagon
+    elif len(approx)==6:
+        print "hexagon"
+        cv2.drawContours(img,[cnt],0,(47,255,173),-1)   # light green for hexa
+    elif len(approx)==7:
+        print "heptagon"
+        cv2.drawContours(img,[cnt],0,(0,153,153),-1)    # dark green for hepta
+    elif len(approx)==8:
+        print "octagon"
+        cv2.drawContours(img,[cnt],0,(226,43,138),-1)      # purple for octogon
+    elif len(approx) == 9:
+        print "nonagon"
+        cv2.drawContours(img,[cnt],0,(255,255,0),-1)
+    elif len(approx) == 10:
+        print "nonagon"
+        cv2.drawContours(img,[cnt],0,(255,0,0),-1)    # blue for star
+    else:
+        print "circle"
+        cv2.drawContours(img,[cnt],0,(128,128,128),-1)  # white for circle
+
+cv2.imshow('gray',gray)
+cv2.imshow('shapes',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- RoboSUB_ImageProcess/src/main/video_filter_underwater.py
+++ RoboSUB_ImageProcess/src/main/video_filter_underwater.py
@@ -12,12 +12,15 @@ video = cv2.VideoCapture(1)
 underwater_color = (255, 0, 0)
 
 while True:
-    _, frame = video.read()
-    
-    img_height, img_width, img_channel = frame.shape
-
     print "still going"
     
+    # get feed
+    frame = video.read()[1]
+    
+    # get dimensions of original feed
+    img_height, img_width, img_channel = frame.shape
+
+    # gray filter for testing
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     # create two copies of the original image -- one for
@@ -33,12 +36,12 @@ while True:
     cv2.addWeighted(overlay, 0.3, underwater_img, 0.8,
         0, underwater_img)
 
-    
+    # display result
     cv2.imshow('original', frame)   # original
     cv2.imshow('gray', gray)
-    cv2.imshow('green', underwater_img)
+    cv2.imshow('underwater', underwater_img)
     
-    # needed to run loop
+    # needed to run loop, listens for keyboard events
     k = cv2.waitKey(30)
     
     # if ESC or spacebar is pressed, exit
